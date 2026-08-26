4968817703b1520f84cbd07e50db2d80f3ffaf7e
Ramavath Lakshmandu Naik <ramavathlakshman0@gmail.com>
2026-08-26 11:04:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Search/binary-search.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public boolean binarySearch(int[] arr, int k) 
+    {
+        // code here
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {
+           int mid=(high+low)/2;
+            if(arr[mid]==k)
+            {
+               return true; 
+             }else if(arr[mid]>k)
+             {
+               high=mid-1;
+            }else 
+            {
+               low=mid+1;
+             }
+        
+        }
+         return false;
+    }
+   
+}
